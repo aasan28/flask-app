17ed0f84bc7d7ea7bdadd94921334ba45cd9cc55
aasan42 <aasan0442@gmail.com>
2025-10-20 12:17:34 +0900

    なんやようわからんけど

--- flaskr/blogs.py
+++ flaskr/blogs.py
@@ -1,14 +1,16 @@
+# ルーティング
+# /blogs にHTTPメソッドがGETでアクセスしたblogs関数を実行する
 from flask import Blueprint, render_template
-from flaskr.db import query_db
+# models.pyのBlogクラスをインポート
+from flaskr.models import Blog
 
 blog_bp = Blueprint("blog", __name__)
 
-# ルーティング
-# /blogs にHTTPメソッドがGETでアクセスしたblogs関数を実行する
 @blog_bp.route("/blogs")
 def blogs():
-   # SQLを使ってデータベースからデータを引き出す（配列の配列の形で帰ってくる）
-    blogs = query_db("SELECT * FROM blogs")
+    from flask import render_template
+    # Blogテーブルから全てのデータを取得し、作成日時の降順で並び替え
+    blogs = Blog.query.order_by(Blog.created_at.desc()).all()
 
     # テンプレートにblogs変数を渡す
     return render_template('blogs.html', blogs = blogs)
